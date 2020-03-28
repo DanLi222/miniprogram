685defcdf2c77ac005eff1100355b775d0f91954
Dan <anne.leedan@gmail.com>
2020-03-28 10:26:16 -0400

    Do not display `deleted` manufacturers

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,6 +12,7 @@ Page({
     })
   },
   showPicture: function(list){
+    console.log(list)
     list.forEach(brand => {
       wx.cloud.downloadFile({
         fileID: 'cloud://dan-sbsq8.6461-dan-sbsq8-1300940270/brand/' 
@@ -27,11 +28,12 @@ Page({
   },
   getList: function(number){
     for(var i = 0; i <= number; i = i+20){
-      db.collection('manufacturer').skip(i).get({
+      db.collection('manufacturer')
+      .where({deleted_at: ""})
+      .skip(i).get({
         success: v => {
           var list = this.data.manufacturer_list;
           list = list.concat(v.data);
-          console.log(list);
           list = list.sort(function(a, b){
             if(a.pingying < b.pingying) { return -1; }
             if(a.pingying > b.pingying) { return 1; }
@@ -57,7 +59,9 @@ Page({
     //   }
     // });
     var that = this;
-    db.collection('manufacturer').count({
+    db.collection('manufacturer')
+    .where({deleted_at: ""})
+    .count({
       success: function(res){
         var number = res.total;
         that.getList(number);
